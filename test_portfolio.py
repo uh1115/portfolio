afbea0ddf66014e4486ff590980f70946b375328
uh1115 <musman.hamid15@gmail.com>
2021-11-16 21:06:21 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,8 +1,18 @@
 from portfolio import Portfolio
 
-p = Portfolio()
-print(f"Empty portfolio cost: {p.cost()}")
-p.buy("IBM", 100, 176.48)
-print(f"With 100 IBM @ 176.48: {p.cost()}")
-p.buy("HPQ", 100, 36.15)
-print(f"With 100 HPQ @ 36.15: {p.cost()}")
+def test_buy_one_stock():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    assert p.cost() == 17648.0
+
+def test_buy_two_stocks():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    p.buy("HPQ", 100, 36.15)
+    assert p.cost() == 21263.0
+
+def test_empty():
+    p = Portfolio()
+    p.buy("IBM", 0, 0)
+    p.buy("HPQ", 0, 0)
+    assert p.cost() == 0
